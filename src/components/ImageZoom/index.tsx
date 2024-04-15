ba766c80c22c05b87ade143103068863842036f9
Adrian Krzeminski <adrian@fictionlab.pl>
2024-04-15 15:29:16 +0200

    Add automatic HTML img attributes & add default style to ImageZoom

--- src/components/ImageZoom/index.tsx
+++ src/components/ImageZoom/index.tsx
@@ -5,10 +5,11 @@ import styles from './styles.module.css';
 export interface ImageZoomProps extends ComponentProps<'img'> {
   caption?: string;
   allowZoom?: boolean;
+  figStyle?: object;
 }
 
 export default function ImageZoom(props: ImageZoomProps): JSX.Element {
-  const { allowZoom, caption, ...propsRest } = props;
+  const { allowZoom, caption, figStyle, ...propsRest } = props;
   const zoomRef = useRef<Zoom | null>(null);
 
   function getZoom() {
@@ -19,19 +20,26 @@ export default function ImageZoom(props: ImageZoomProps): JSX.Element {
     }
     return zoomRef.current;
   }
-
-  const attachZoom: RefCallback<HTMLImageElement> = (node) => {
+  
+  const imageRef: RefCallback<HTMLImageElement> = (node) => {
     const zoom = getZoom();
     if (node && allowZoom != false) {
       zoom.attach(node);
     } else {
       zoom.detach();
     }
+
+    if (node) {
+      if (!node.getAttribute('loading')) node.setAttribute('loading', 'lazy');
+      const { naturalWidth, naturalHeight } = node;
+      node.setAttribute('width', String(naturalWidth));
+      node.setAttribute('height', String(naturalHeight));
+    }
   };
 
   return (
-    <figure className={styles.figure}>
-      <img {...propsRest} ref={attachZoom} />
+    <figure className={styles.figure} style={figStyle}>
+      <img className={styles.image} {...propsRest} ref={imageRef} />
       {caption && (
         <figcaption className={styles.figCaption}>{caption}</figcaption>
       )}
