3a41ee8869a1cc68044cf9580de09b2f6a5b3527
Adrian Krzeminski <adrian@fictionlab.pl>
2024-04-16 16:13:52 +0200

    Fix spelling & formatting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -194,9 +194,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: [
-        'bash',
-      ]
+      additionalLanguages: ['bash'],
     },
     announcementBar: {
       id: 'announcementBar_work-in-progress',
