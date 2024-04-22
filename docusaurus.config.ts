45fde90ba641992b482f679fc25bcb69d7444286
Adrian Krzeminski <adrian@fictionlab.pl>
2024-04-22 16:34:19 +0200

    Auto Collapse sidebar categories to reduce clutter

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,6 +69,12 @@ const config: Config = {
   ],
 
   themeConfig: {
+    docs: {
+      sidebar: {
+        hideable: true,
+        autoCollapseCategories: true,
+      },
+    },
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: false,
